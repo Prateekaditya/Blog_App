4cab90580b1c43135c81159ea7a047a12621a4ce
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-06-20 16:59:09 +0530

    ed

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,23 +6,23 @@ const About = () => {
       <div className='flex flex-col'>
         <div className='w-full h-[220px] relative'> 
           <Image  className="object-cover grayscale" fill={true} src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3" alt="mainarea" />
-          <div className='absolute bottom-[20px] left-[20px] bg-[#ed6c4f] rounded-2xl p-[10px]'>
-            <h1 className='font-bold text-3xl'>Digital StoryTellers</h1>
-            <h2 className='text-xl'>Handcrafted digital award winning Experinces</h2>
+          <div className='absolute bottom-[15px] w-[250px] md:w-[430px] left-[2px] md:bottom-[20px] md:left-[20px] bg-[#ed6c4f] rounded-2xl p-[10px]'>
+            <h1 className='font-bold text-xl md:text-3xl'>Digital StoryTellers</h1>
+            <h2 className='text-[15px] md:text-xl'>Handcrafted digital award winning Experinces</h2>
           </div>
         </div>
-        <div className='flex flex-row gap-[100px]'>
-          <div className='flex-1 flex flex-col gap-[20px] mt-[10px]'>
-            <h2 className='text-4xl font-bold'>Who are we?</h2>
-            <p className='text-[15px] font-[300] text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere rutrum purus, sit amet tempus ex. Nam feugiat nisi mauris. Curabitur a sem quis erat elementum dictum at id magna. Quisque ac 
+        <div className='flex flex-row gap-[30px] md:gap-[100px]'>
+          <div className='flex-1 flex flex-col gap-[10px] mt-[10px]'>
+            <h2 className='text-[18px]/6 md:text-4xl font-bold'>Who are we?</h2>
+            <p className='text-[11px] md:text-[15px] font-[300] text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere rutrum purus, sit amet tempus ex. Nam feugiat nisi mauris. Curabitur a sem quis erat elementum dictum at id magna. Quisque ac 
               <br/>
               <br/>
               elit vitae lacus imperdiet luctus. Proin leo sem, eleifend a iaculis eget, egestas sed sem. Donec at ultricies neque. Etiam imperdiet pharetra metus ac ultrices. Pellentesque ac neque sapien. Sed quis velit quis diam tempor mattis. Mauris nulla lacus, semper a rhoncus et, facilisis in ante.
             </p>
           </div>
           <div className='flex-1 flex flex-col gap-[10px] mt-[10px]'>
-            <h2 className='text-4xl font-bold'>What we do?</h2>
-            <p className='text-[15px] font-[300] text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere rutrum purus, sit amet tempus ex. Nam feugiat nisi mauris. Curabitur a sem quis erat elementum dictum at id magna. Quisque ac elit vitae lacus imperdiet luctus.
+            <h2 className='text-[18px]/6 md:text-4xl font-bold'>What we do?</h2>
+            <p className='text-[11px] md:text-[15px] font-[300] text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere rutrum purus, sit amet tempus ex. Nam feugiat nisi mauris. Curabitur a sem quis erat elementum dictum at id magna. Quisque ac elit vitae lacus imperdiet luctus.
             <br/>
               <br/>-Creative Illustraion
               <br/>
